49e09669ed332b47048b650edf237228473321bd
Bob Lail <bob.lail@cph.org>
2015-02-22 16:59:49 -0600

    [refactor] Refactored Navigation Renderer (1m)

--- config/initializers/add_navigation_renderer.rb
+++ config/initializers/add_navigation_renderer.rb
@@ -1,3 +1,5 @@
-# Houston.config.add_navigation_renderer :support_form do
-#  render_nav_link "SupportForm", Houston::SupportForm::Engine.routes.url_helpers.root_path, icon: "fa-thumbs-up"
-# end
+Houston.config.add_project_feature :support_form do
+  name "Support"
+  icon "fa-bullhorn"
+  path { |project| Houston::SupportForm::Engine.routes.url_helpers.project_support_path(project) }
+end
